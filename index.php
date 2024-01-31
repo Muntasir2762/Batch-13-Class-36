ac9e9e98961e88eea3ac80f54e6bc9e6e3f90bb9
Munotasir2762 <muntasirpranto2762@gmail.com>
2024-02-01 00:58:15 +0600

    PHP loop & function

---
+++ index.php
@@ -0,0 +1,66 @@
+<?php
+
+// $x = 20;
+
+// if($x == 10){
+//     echo 'The value is=10';
+// }
+
+// else{
+//     echo 'The value is not=10';
+// }
+
+// $x = 'red';
+
+// switch($x){
+// case 'red';
+// echo 'The color is red';
+// break;
+
+// case 'green';
+// echo 'The color is green';
+// break;
+
+// case 'blue';
+// echo 'The color is green';
+// break;
+
+// default:
+// echo 'None of the color is existing here';
+// break;
+// }
+
+
+// $array = [10, 20, 30, 40,34];   //index array
+
+// echo count($array). '<br>';
+
+// for ($i=0; $i<=4; $i++){
+//     echo $array[$i].'<br>';
+// }
+
+
+// foreach($array as $value){
+//     echo $value.'<br>';
+// }
+
+
+#include <stdio.h>
+int main() {
+
+rtjutrj
+
+    return 0;
+ }
+
+
+function show ($number1, $number2, $number3){
+    return 'Hello world';  //30
+}
+
+echo show(10, 20, 30);
+// $result = show(10, 20);  //30
+
+// echo $result;
+
+?>
